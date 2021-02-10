7e4d20f668708797e8281c0e6ffe57edc92d38f3
saschahaselhuhn <sascha.haselhuhn@gmail.com>
2021-02-10 17:40:38 +0100

    Clear Fix

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -29,30 +29,59 @@ export type APICharacters = {
   results: APICharacter[];
 };
 
-export async function getCharacter(id: number) {
-  const response = await fetch(
-    `https://rickandmortyapi.com/api/character/${id}`
-  );
-  const result = (await response.json()) as APICharacter;
-  const character = {
-    imgSrc: result.image,
-    name: result.name,
-    status: result.status,
-    species: result.species,
-    origin: { name: result.origin.name },
+export type Character = {
+  imgSrc: string;
+  name: string;
+  status: "Alive" | "Dead" | "unknown";
+  species: string;
+  origin: {
+    name: string;
   };
-  return character;
-}
+};
 
-export async function getCharacters() {
-  const response = await fetch(`https://rickandmortyapi.com/api/character/`);
-  const result = (await response.json()) as APICharacters;
-  const characters = result.results.map((apiCharacter) => ({
+function convertToCharacter(apiCharacter: APICharacter): Character {
+  return {
     imgSrc: apiCharacter.image,
     name: apiCharacter.name,
     status: apiCharacter.status,
     species: apiCharacter.species,
-    origin: { name: apiCharacter.origin.name },
-  }));
+    origin: {
+      name: apiCharacter.origin.name,
+    },
+  };
+}
+
+export async function getCharacter(id: number) {
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/character/${id}`
+  );
+  if (!response.ok) {
+    const result = await response.json();
+    return {
+      imgSrc: "",
+      name: result.error,
+      status: "Dead",
+      species: "Human",
+      origin: {
+        name: "Internet",
+      },
+    };
+  }
+  const result = (await response.json()) as APICharacter;
+  const character = convertToCharacter(result);
+  return character;
+}
+
+export async function getCharacters(name?: string) {
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/character/${name ? `?name=${name}` : ""}`
+  );
+  if (!response.ok) {
+    return [];
+  }
+  const result = (await response.json()) as APICharacters;
+  const characters = result.results.map((apiCharacter) =>
+    convertToCharacter(apiCharacter)
+  );
   return characters;
 }
